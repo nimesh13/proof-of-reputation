24d2d1fb75e4c260e62d2caba05ab86d4d0e9260
Nimesh Doolani <nimesh@Nimeshs-MacBook-Air.local>
2021-11-27 23:28:04 -0800

    full working implementation

--- client.js
+++ client.js
@@ -7,6 +7,7 @@ let ReputationBlockchain = require('./blockchain.js');
 let rand = require('./rand');
 
 let identityCount = 0;
+let numRounds = 0;
 
 module.exports = class ReputationClient extends Client {
 
@@ -16,10 +17,15 @@ module.exports = class ReputationClient extends Client {
     this.identity = identityCount;
     identityCount += 1;
 
+    this.on(ReputationBlockchain.START_VOTING, this.voteWinner);
+    this.on(ReputationBlockchain.VOTE_WINNER, this.receiveVote);
+    this.on(ReputationBlockchain.PROOF_FOUND1, this.receiveProof);
+    this.on(ReputationBlockchain.VOTE_BLOCK, this.receiveVoteBlock);
+
   }
 
   get reputationScore() {
-    return this.lastConfirmedBlock.balanceOf(this.address);
+    return this.lastConfirmedBlock.reputationOf(this.address);
   }
 
   showAllBalances() {
@@ -31,40 +37,49 @@ module.exports = class ReputationClient extends Client {
 
   initialize() {
 
-    this.startNewSearch();
+    // if (numRounds == 7) process.exit(0);
 
-    this.on(ReputationBlockchain.START_VOTING, this.voteWinner);
-    this.on(ReputationBlockchain.VOTE_WINNER, this.receiveVote);
+    this.startNewSearch();
 
     setTimeout(() => this.emit(ReputationBlockchain.START_VOTING), 0);
   }
 
   startNewSearch() {
-    this.currentBlock = ReputationBlockchain.makeBlock(this.address, this.lastBlock);
+
+    this.currentBlock = ReputationBlockchain.makeBlock(this.address, this.lastConfirmedBlock);
+    // if (numRounds === 5) console.log('Current block', this.currentBlock);
+    // console.log(this.name, 'Starting new search');
   }
 
   voteWinner() {
-    this.numPlayers = this.lastConfirmedBlock.reputations.size;
+    this.numPlayers = this.currentBlock.reputations.size;
 
     let number = rand.nextInt(this.numPlayers);
 
-    this.announceProof({ id: this.address, vote: number });
+    this.announceVote({ id: this.address, vote: number });
   }
 
   /**
    * Broadcast the block, with a valid proof included.
    */
-  announceProof(msg) {
-    // console.log('Message: ', msg);
+  announceVote(msg) {
     this.net.broadcast(ReputationBlockchain.VOTE_WINNER, msg);
   }
 
   receiveVote(msg) {
     this.currentBlock.voteWinnerMap.set(msg.id, msg.vote);
 
-    if (this.currentBlock.voteWinnerMap.size == this.numPlayers) {
-      this.determineWinner()
+    if (this.currentBlock.voteWinnerMap.size === this.numPlayers) {
+      this.determineWinner();
+      if (this.currentBlock.winner === this.address) {
+        this.announceProof();
+      }
     }
+  }
+
+  announceProof() {
+    let msg = { proof: rand.nextInt(200) };
+    this.net.broadcast(ReputationBlockchain.PROOF_FOUND1, msg);
   }
 
   determineWinner() {
@@ -74,16 +89,122 @@ module.exports = class ReputationClient extends Client {
       sum += share;
     });
     let winnerID = sum % this.numPlayers;
-    this.currentBlock.winner = this.getWinnerName(winnerID);
+    this.currentBlock.winner = this.getClientName(winnerID);
 
     console.log(`${this.name} announces ${this.currentBlock.winner} as the winner`);
   }
 
-  getWinnerName(winnerID) {
-    let winnerAddress = this.net.clientIdentities.get(winnerID);
+  getClientName(index) {
+    let winnerAddress = this.net.clientIdentities.get(index);
 
     for (let [_, client] of this.net.clients) {
-      if (winnerAddress === client.address) return client.name;
+      if (winnerAddress === client.address) return client.address;
     }
+  }
+
+  receiveProof(msg) {
+    this.currentBlock.proof = msg.proof;
+    this.voteBlock();
+  }
+
+  voteBlock() {
+    if (this.currentBlock.winner !== this.address) {
+      let msg = { id: this.address, vote: this.calculateVote() };
+      this.net.broadcast(ReputationBlockchain.VOTE_BLOCK, msg);
+    }
+  }
+
+  calculateVote() {
+
+    //add reputation class and getter to fetch the votes
+    let num = rand.nextInt(2);
+
+    if (num) return 'VALID';
+    return 'INVALID';
+  }
+
+  receiveVoteBlock(msg) {
+
+    this.currentBlock.votesMap.set(msg.id, msg.vote);
+
+    if (this.currentBlock.votesMap.size === (this.numPlayers - 1)) {
+      this.currentBlock.decision = this.determineBlockStatus();
+
+      this.updateReputation();
+      
+      this.updateBlocks();
+
+      this.initialize();
+
+    }
+  }
+
+  determineBlockStatus() {
+    let status = {
+      'VALID': 0,
+      'INVALID': 0
+    };
+
+    for (let [client, vote] of this.currentBlock.votesMap) {
+      let repValue = this.currentBlock.reputations.get(client);
+      if (vote == 'VALID') status['VALID'] += repValue;
+      else status['INVALID'] += repValue;
+    }
+
+    if (status['VALID'] > status['INVALID']) return 'VALID';
+    return 'INVALID';
+  }
+
+  updateReputation() {
+    let decision = this.currentBlock.decision;
+    let winner = this.currentBlock.winner;
+    let voteMap = this.currentBlock.votesMap;
+
+    for (let [client, vote] of voteMap) {
+      let repValue = this.currentBlock.reputationOf(client);
+      if (vote === decision) repValue += 10;
+      else repValue -= 10;
+      this.currentBlock.reputations.set(client, repValue);
+    }
+
+    let winnerRepValue = this.currentBlock.reputations.get(winner);
+    if (decision === 'VALID') winnerRepValue += 10;
+    else winnerRepValue -= 10;
+
+    this.currentBlock.reputations.set(winner, winnerRepValue);
+
+  }
+
+  updateBlocks() {
+
+    let block = this.currentBlock;
+
+    // Storing the block.
+    this.blocks.set(block.id, block);
+
+    // If it is a better block than the client currently has, set that
+    // as the new currentBlock, and update the lastConfirmedBlock.
+
+    if (this.lastBlock.chainLength < block.chainLength) {
+    this.lastBlock = block;
+    this.setLastConfirmed();
+    }
+
+
+  }
+
+  setLastConfirmed() {
+    let block = this.lastBlock;
+    // console.log('Last Block: ', this.lastBlock);
+
+    // let confirmedBlockHeight = block.chainLength - ReputationBlockchain.CONFIRMED_DEPTH;
+    // if (confirmedBlockHeight < 0) {
+    //   confirmedBlockHeight = 0;
+    // }
+    // while (block.chainLength > confirmedBlockHeight) {
+    //   block = this.blocks.get(block.prevBlockHash);
+    // }
+    this.lastConfirmedBlock = block;
+
   }
 }
